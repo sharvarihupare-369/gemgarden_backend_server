99a8c2b97d85f259f93a3c55d0165bbfe55030a1
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-08-29 13:00:14 +0530

    add

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const jsonServer = require('json-server');
+require("dotenv").config()
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const cors = require("cors")
 
+server.use(cors())
 server.use(middlewares);
 server.use(router);
+
 server.listen(process.env.PORT || 8080, () => {
   console.log('JSON Server is running');
 });
